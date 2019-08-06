e93c45f51af16758a10d3f3f60d15a953a10579c
张煜 <zephyrjung@126.com>
2019-08-06 18:16:04 +0800

    some change

--- boot.sh
+++ boot.sh
@@ -4,10 +4,12 @@ SOURCE_PATH="/Users/yu.zhang/Documents/GithubSpace/MarkSpace"
 PUBLISH_PATH="/Users/yu.zhang/Documents/GithubSpace/zephyrjung.github.io"
 
 publicFiles=$(find $SOURCE_PATH -name '*.md' | grep '.public.md')
+index=0
 
 for f in $publicFiles
 do
-  cp -rf $f $WORK_PATH/template/source/
+#  cp $f $WORK_PATH/template/source/$(date"+%Y%m%d"$index)
+  index=index+1
 done
 
 cd $WORK_PATH
@@ -15,3 +17,5 @@ cd $WORK_PATH
 rm -rf $WORK_PATH/template/public
 
 ./ink publish ./template
+
+cp -rf $WORK_PATH/template/public/* ~/Documents/GithubSpace/zephyrjung.github.io/
